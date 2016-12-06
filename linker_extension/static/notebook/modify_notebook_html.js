0d729031dc4ce247fcccc943fd889566a7827446
Louise Davies <louise.davies@stfc.ac.uk>
2016-12-06 14:44:53 +0000

    Modified so individual files arent't extensions. Also added some JQuery to inject our custom css into the head.

--- linker_extension/static/notebook/modify_notebook_html.js
+++ linker_extension/static/notebook/modify_notebook_html.js
@@ -1,7 +1,7 @@
 define(['base/js/namespace','base/js/utils'],function(Jupyter,utils){
 
-	function load_ipython_extension(){
-		//applies to all pages - need to repeat for tree and edit
+	var load = function() {
+        //applies to all pages - need to repeat for tree and edit
         console.log('Modify notebook html loaded');
         var header_container = $('#header-container').detach();
         var header_container_container = $('<div/>').attr("id","header-container-container").addClass("container");
@@ -11,9 +11,16 @@ define(['base/js/namespace','base/js/utils'],function(Jupyter,utils){
         var file_tree_button = $('<a/>').addClass("btn btn-default btn-sm navbar-btn pull-right").css("margin-right","2px").css("margin-left","2px").attr("href",Jupyter.notebook.base_url).text("File Tree");
         $('#login_widget').after(file_tree_button);
         
-		$('#ipython_notebook').children("a").attr("href","https://www.stfc.ac.uk/");
+    	$('#ipython_notebook').children("a").attr("href","https://www.stfc.ac.uk/");
 
         //notebook page specific modifications
+        $('head').append(
+            $('<link>')
+            .attr('rel', 'stylesheet')
+            .attr('type', 'text/css')
+            .attr('href', Jupyter.notebook.base_url + 'nbextensions/linker_extension/notebook/notebook_style.css')
+        );
+
         var menubar_container = $('#menubar-container').detach();
         var menubar_container_container = $('<div/>').attr("id","menubar-container-container").addClass("container");
         menubar_container_container.append(menubar_container);
@@ -38,7 +45,7 @@ define(['base/js/namespace','base/js/utils'],function(Jupyter,utils){
         	   	   .append($("<li/>").attr("id","select_data").append($("<a/>").attr("href","#").text("Select associated data")))
         		   .append($("<li/>").attr("id","view_data").append($("<a/>").attr("href","#").text("View associated data")))
         		   .append($("<li/>").attr("id","upload_data").append($("<a/>").attr("href","#").text("Upload associated data")));
-    
+
         $("#download_pdf").remove();
         var new_pdf_menu_item = $("<li>").attr("id","#download_pdf").append($("<a/>").attr("href","#").text("PDF via LaTeX (.pdf)"));
 
@@ -63,11 +70,5 @@ define(['base/js/namespace','base/js/utils'],function(Jupyter,utils){
         });
 
         $("#download_rst").after(new_pdf_menu_item);
-
-
-    }
-
-    return {
-        load_ipython_extension: load_ipython_extension
     };
 });
--- linker_extension/static/tree/modify_tree_html.js
+++ linker_extension/static/tree/modify_tree_html.js
@@ -1,8 +1,7 @@
-define(['base/js/namespace','nbextensions/linker_extension/custom_utils'],function(Jupyter,custom_utils){
+define(['base/js/namespace','../custom_utils'],function(Jupyter,custom_utils){
 
-	function load_ipython_extension(){
-		//applies to all pages - need to repeat for tree and edit
-        console.log('Modify tree html loaded');
+	var load = function() {
+        //applies to all pages - need to repeat for notebook and edit
         var header_container = $('#header-container').detach();
         var header_container_container = $('<div/>').attr("id","header-container-container").addClass("container");
         header_container_container.append(header_container);
@@ -11,18 +10,25 @@ define(['base/js/namespace','nbextensions/linker_extension/custom_utils'],functi
         var file_tree_button = $('<a/>').addClass("btn btn-default btn-sm navbar-btn pull-right").css("margin-right","2px").css("margin-left","2px").attr("href",Jupyter.session_list.base_url).text("File Tree");
         $('#login_widget').after(file_tree_button);
         
-		$('#ipython_notebook').children("a").attr("href","https://www.stfc.ac.uk/");
+    	$('#ipython_notebook').children("a").attr("href","https://www.stfc.ac.uk/");
 
         //tree page specific modifications
+        $('head').append(
+            $('<link>')
+            .attr('rel', 'stylesheet')
+            .attr('type', 'text/css')
+            .attr('href', Jupyter.session_list.base_url + 'nbextensions/linker_extension/tree/tree_style.css')
+        );
+
+        $('head').prepend($('<link>').attr("lel",'lelelelleellel'));
+
         var bundle_instructions = $("<div/>").addClass("bundle-instructions").text("Select files and directories to associate with your chosen notebook. Note: you cannot associate notebooks with other notebooks.\n");
         $('.dynamic-instructions').after(bundle_instructions);
 
         var bundle_button = $('<button/>').addClass("bundle-button btn btn-default btn-xs").attr("title","Associated the selected files with " + sessionStorage.getItem("bundle")).text("Bundle");
         var cancel_button = $('<button/>').addClass("bundle-cancel-button btn btn-default btn-xs").attr("title","Stop associating files and return to the regular tree").text("Cancel");
         $('.dynamic-buttons').prepend(cancel_button).prepend(bundle_button);
-    }
-
-    return {
-        load_ipython_extension: load_ipython_extension
     };
+
+    module.exports = {load: load}; 
 });
