4a0ff5efc58bf70ead3734a3193d6e5d68aa6147
Louise Davies <louise.davies@stfc.ac.uk>
2016-12-12 12:45:46 +0000

    Added function to make ldap login request

--- linker_extension/static/custom_contents.js
+++ linker_extension/static/custom_contents.js
@@ -92,6 +92,18 @@ define(['base/js/namespace','base/js/utils'], function(Jupyter,utils){
         return utils.promising_ajax(url, settings);
     };
 
+    var ldap_auth = function(options) {
+        var url = utils.url_path_join.apply(null,[Jupyter.notebook.base_url, '/ldap']);
+        var settings = {
+            processData : false,
+            cache : false,
+            type : "POST",
+            data: options,
+            contentType: 'application/json',
+        };
+        return utils.promising_ajax(url, settings);
+    };
+
     module.exports = {
         load_ipython_extension: load_ipython_extension,
         sword_get_servicedocument: sword_get_servicedocument,
@@ -101,6 +113,7 @@ define(['base/js/namespace','base/js/utils'], function(Jupyter,utils){
         sword_delete_item: sword_delete_item,
         dspace_new_item: dspace_new_item,
         upload_data: upload_data,
-        sword_get_bitstream_data: sword_get_bitstream_data
+        sword_get_bitstream_data: sword_get_bitstream_data,
+        ldap_auth: ldap_auth,
     };
 });
