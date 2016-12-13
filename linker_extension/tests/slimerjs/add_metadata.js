d8017dfc1fdc34cb28c6367f663ab5e01b39302e
Louise Davies <louise.davies@stfc.ac.uk>
2016-12-13 16:16:22 +0000

    Extended test to test the validators, to test all the fields and test the deletion of authors and urls

--- linker_extension/tests/slimerjs/add_metadata.js
+++ linker_extension/tests/slimerjs/add_metadata.js
@@ -17,27 +17,143 @@ casper.notebook_test(function() {
     this.waitUntilVisible(".modal-body");
     this.wait(200);
 
-    //Add some test metadata
+    //check that certain fields are required 
+    this.waitForSelector('#next');
+    this.thenClick('#next');
+
+    this.then(function() {
+        this.test.assertExists("#title-missing-error","Title missing error exists");
+        this.test.assertExists("#abstract-missing-error","Abstract missing error exists");
+        this.test.assertExists("#year-missing-error","Year missing error exists");
+    });
+
+    //testing the date validation
     this.then(function() {
         this.fill('form#add_metadata_form > fieldset#fields1', {
             'title': "My Title",
             'abstract': "My abstract",
-            'year': '1995',
-
+            'year': '4000',
         });
-    }); //TODO: check all the fields work. Also need to check the validators.
-    this.waitForSelector('#next');
+    });
     this.thenClick('#next');
-    this.wait(300);
-    this.waitForSelector("#collections_loaded"); //feels dirty...
     this.then(function() {
-        this.fill('form#add_metadata_form > fieldset#fields2', {
-            'repository': "8" //8 is SCD
+        this.test.assertExists("#invalid-year-error","Upper bound on year limit working");
+    });
+
+
+    this.then(function() {
+        this.fill('form#add_metadata_form > fieldset#fields1', {
+            'title': "My Title",
+            'abstract': "My abstract",
+            'year': '1000',
         });
     });
-    this.thenEvaluate(function() {
-        console.log($(document.body).html());
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertExists("#invalid-year-error","Lower bound on year limit working");
     });
+
+    this.then(function() {
+        this.fill('form#add_metadata_form > fieldset#fields1', {
+            'title': "My Title",
+            'abstract': "My abstract",
+            'year': '2000',
+            'day': '31',
+        });
+    });
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertExists("#month-missing-error","Month missing error exists");
+    });
+
+    this.then(function() {
+        this.fill('form#add_metadata_form > fieldset#fields1', {
+            'title': "My Title",
+            'abstract': "My abstract",
+            'year': '2000',
+            'month': '2',
+            'day': '31',
+        });
+    });
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertExists("#invalid-day-error","Date logic working for rejecting invalid combos");
+    });
+
+    this.then(function() {
+        this.fill('form#add_metadata_form > fieldset#fields1', {
+            'title': "My Title",
+            'abstract': "My abstract",
+            'year': '1900', //not a leap year
+            'month': '2',
+            'day': '29',
+        });
+    });
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertExists("#invalid-day-error","Leap year logic working for rejecting invalid combos");
+    });
+
+    this.then(function() {
+        this.fill('form#add_metadata_form > fieldset#fields1', {
+            'title': "My Title",
+            'abstract': "My abstract",
+            'year': '2000', //is a leap year!
+            'month': '2',
+            'day': '29',
+        });
+    });
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertVisible("#fields2","Leap year logic working for accepting valid combos");
+    });
+
+    this.thenClick('#previous');
+    this.waitForSelector('#add-author-button');
+    this.thenClick("#add-author-button");
+    this.thenClick("#add-author-button"); //create two extra author fields - we'll end up leaving one blank
+
+    //Add some test metadata
+    this.then(function() {
+        this.fillSelectors('form#add_metadata_form > fieldset#fields1', { //need to use fill selectors to fill in the authors
+            '#title': "My Title",
+            '#abstract': "My abstract",
+            '#year': '1995',
+            '#month': '8',
+            '#day': '20',
+            '#language': 'en',
+            '#tags': 'tag1\ntag2',
+            '#author-last-name-0': 'Davies',
+            '#author-first-name-0': 'Louise',
+            '#author-last-name-2': "S'chn T'gai",
+            '#author-first-name-2': 'Spock',
+        });
+    }); 
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertVisible("#fields2","Valid data has been accepted");
+    });
+
+    this.waitForSelector("#collections_loaded"); //feels dirty...
+
+    this.thenClick('#next');
+    this.then(function() {
+        this.test.assertExists("#repository-missing-error","Repository missing error exists");
+    });
+
+    this.waitForSelector("#add-url-button");
+    this.thenClick("#add-url-button");
+    this.thenClick("#add-url-button"); //again, create two extra boxes but we'll only use one
+
+    this.then(function() {
+        this.fillSelectors('form#add_metadata_form > fieldset#fields2', { //need to use fillSelectors for the referencedBy urls
+            '#publisher': 'test publisher',
+            '#citation': 'test citation',
+            '#referencedBy-0': "URL1",
+            '#referencedBy-2': "URL2",
+            '#repository': "8" //8 is SCD
+        });
+    }); //TODO: add funders and sponsors if we can use them
     this.thenClick('#next');
     this.wait(300);
 
@@ -51,14 +167,26 @@ casper.notebook_test(function() {
                 return {
                     title: "",
                     abstract: "",
-                    year: '',
-                    repository: '',
+                    date:"",
+                    language: '',
+                    tags: [],
+                    authors: [],
+                    publisher: '',
+                    citation: '',
+                    referencedBy: [],
+                    repository: "" //8 is SCD
                 };
             } else {
                 return {
                     title: md.reportmetadata.title,
                     abstract: md.reportmetadata.abstract,
                     date: md.reportmetadata.date,
+                    language: md.reportmetadata.language,
+                    tags: md.reportmetadata.tags,
+                    authors: md.reportmetadata.authors,
+                    publisher: md.reportmetadata.publisher,
+                    citation: md.reportmetadata.citation,
+                    referencedBy: md.reportmetadata.referencedBy,
                     repository: md.reportmetadata.repository,
                 };
             }
@@ -66,7 +194,13 @@ casper.notebook_test(function() {
         });
         this.test.assertEquals(metadata.title,"My Title","Title has been set correctly");
         this.test.assertEquals(metadata.abstract,"My abstract","Abstract has been set correctly");
-        this.test.assertEquals(metadata.date,"1995","Date has been set correctly");
+        this.test.assertEquals(metadata.date,"1995-08-20","Date has been set correctly");
+        this.test.assertEquals(metadata.language,"en","Language has been set correctly");
+        this.test.assertEquals(metadata.tags,["tag1","tag2"],"Tags have been set correctly");
+        this.test.assertEquals(metadata.authors,[["Davies","Louise"],["S'chn T'gai","Spock"]],"Authors have been set correctly");
+        this.test.assertEquals(metadata.publisher,"test publisher","Publisher has been set correctly");
+        this.test.assertEquals(metadata.citation,"test citation","Citation has been set correctly");
+        this.test.assertEquals(metadata.referencedBy,["URL1","URL2"],"ReferencedBy had been set correctly");
         this.test.assertEquals(metadata.repository,"8","Repository has been set correctly");
     });
 
@@ -122,27 +256,48 @@ casper.notebook_test(function() {
 
     //check that our metadata has been saved to the notebook metadata
     this.then(function() {
+        var that = this;
         var metadata = this.evaluate(function() {
             var md = Jupyter.notebook.metadata;
             if(!md.hasOwnProperty("reportmetadata")) {
-                    return {
+                __utils__.echo("No reportmetadata");
+                return {
                     title: "",
                     abstract: "",
-                    year: '',
-                    repository: '',
+                    date:"",
+                    language: '',
+                    tags: [],
+                    authors: [],
+                    publisher: '',
+                    citation: '',
+                    referencedBy: [],
+                    repository: "" //8 is SCD
                 };
             } else {
                 return {
                     title: md.reportmetadata.title,
                     abstract: md.reportmetadata.abstract,
                     date: md.reportmetadata.date,
+                    language: md.reportmetadata.language,
+                    tags: md.reportmetadata.tags,
+                    authors: md.reportmetadata.authors,
+                    publisher: md.reportmetadata.publisher,
+                    citation: md.reportmetadata.citation,
+                    referencedBy: md.reportmetadata.referencedBy,
                     repository: md.reportmetadata.repository,
                 };
             }
+            
         });
         this.test.assertEquals(metadata.title,"My Title","Title has been saved correctly");
         this.test.assertEquals(metadata.abstract,"My abstract","Abstract has been saved correctly");
-        this.test.assertEquals(metadata.date,"1995","Date has been saved correctly");
+        this.test.assertEquals(metadata.date,"1995-08-20","Date has been saved correctly");
+        this.test.assertEquals(metadata.language,"en","Language has been saved correctly");
+        this.test.assertEquals(metadata.tags,["tag1","tag2"],"Tags have been set correctly");
+        this.test.assertEquals(metadata.authors,[["Davies","Louise"],["S'chn T'gai","Spock"]],"Authors have been saved correctly");
+        this.test.assertEquals(metadata.publisher,"test publisher","Publisher has been saved correctly");
+        this.test.assertEquals(metadata.citation,"test citation","Citation has been saved correctly");
+        this.test.assertEquals(metadata.referencedBy,["URL1","URL2"],"ReferencedBy had been saved correctly");
         this.test.assertEquals(metadata.repository,"8","Repository has been saved correctly");
     });
 
@@ -161,13 +316,103 @@ casper.notebook_test(function() {
                 titleval: document.getElementById('title').value,
                 abstractval: document.getElementById('abstract').value,
                 yearval: document.getElementById('year').value,
+                monthval: document.getElementById('month').value,
+                dayval: document.getElementById('day').value,
+                tagsval: document.getElementById('tags').value,
+                authorfn0val: document.getElementById('author-first-name-0').value,
+                authorln0val: document.getElementById('author-last-name-0').value,
+                authorfn1val: document.getElementById('author-first-name-1').value,
+                authorln1val: document.getElementById('author-last-name-1').value,
+                languageval: document.getElementById('language').value,
+                publisherval: document.getElementById('publisher').value,
+                citationval: document.getElementById('citation').value,
+                reference0val: document.getElementById('referencedBy-0').value,
+                reference1val: document.getElementById('referencedBy-1').value,
                 repositoryval: document.getElementById('repository').value,
             };
         });
         this.test.assertEquals(vals.titleval,"My Title","Title displays properly in the form once set");
         this.test.assertEquals(vals.abstractval,"My abstract","Abstract displays properly in the form once set");
-        this.test.assertEquals(vals.yearval,"1995","Date displays properly in the form once set");
+        this.test.assertEquals(vals.yearval,"1995","Year displays properly in the form once set");
+        this.test.assertEquals(vals.monthval,"8","Month displays properly in the form once set");
+        this.test.assertEquals(vals.dayval,"20","Day displays properly in the form once set");
+        this.test.assertEquals(vals.tagsval,"tag1\ntag2\n","Tags displays properly in the form once set");
+        this.test.assertEquals(vals.authorfn0val,"Louise","1st author first name displays properly in the form once set");
+        this.test.assertEquals(vals.authorln0val,"Davies","1st author last name displays properly in the form once set");
+        this.test.assertEquals(vals.authorfn1val,"Spock","2nd author first name displays properly in the form once set");
+        this.test.assertEquals(vals.authorln1val,"S'chn T'gai","2nd author last name displays properly in the form once set");
+        this.test.assertEquals(vals.languageval,"en","Language displays properly in the form once set");
+        this.test.assertEquals(vals.publisherval,"test publisher","Publisher displays properly in the form once set");
+        this.test.assertEquals(vals.citationval,"test citation","Citation displays properly in the form once set");
+        this.test.assertEquals(vals.reference0val,"URL1","Reference 0 displays properly in the form once set");
+        this.test.assertEquals(vals.reference1val,"URL2","Reference 1 displays properly in the form once set");
         this.test.assertEquals(vals.repositoryval,"8","Repository displays properly in the form once set");
+    });
+
+    //testing deletion of the multi-field vairables
+    this.waitForSelector('#add-author-button');
+    this.thenClick("#add-author-button");
+    this.thenClick("#add-author-button");
+
+    //Add some test metadata
+    this.then(function() {
+        this.fillSelectors('form#add_metadata_form > fieldset#fields1', {
+            '#author-last-name-2': 'McCoy',
+            '#author-first-name-2': 'Leonard',
+            '#author-last-name-3': "Kirk",
+            '#author-first-name-3': 'James T.',
+        });
+    }); 
+
+    this.thenEvaluate(function() {
+        $('#author-last-name-1').parent().find("button").click();
+        $('#author-last-name-2').parent().find("button").click();
+    });
+
+    this.thenClick('#next');
+
+    this.waitForSelector("#collections_loaded"); //feels dirty...
+
+    this.waitForSelector("#add-url-button");
+    this.thenClick("#add-url-button");
+    this.thenClick("#add-url-button");
+
+    this.then(function() {
+        this.fillSelectors('form#add_metadata_form > fieldset#fields2', { //need to use fillSelectors for the referencedBy urls
+            '#referencedBy-2': "URL3",
+            '#referencedBy-3': "URL4",
+        });
+    });
+
+    this.thenEvaluate(function() {
+        $('#referencedBy-1').parent().find("button").click();
+        $('#referencedBy-2').parent().find("button").click();
+    });
+
+    this.thenClick('#next');
+    this.wait(300);
+
+    //Should be within notebook metadata now.     
+    this.then(function() {
+        var that = this;
+        var metadata = this.evaluate(function() {
+            var md = Jupyter.notebook.metadata;
+            if(!md.hasOwnProperty("reportmetadata")) {
+                __utils__.echo("No reportmetadata");
+                return {
+                    authors: [],
+                    referencedBy: [],
+                };
+            } else {
+                return {
+                    authors: md.reportmetadata.authors,
+                    referencedBy: md.reportmetadata.referencedBy,
+                };
+            }
+            
+        });
+        this.test.assertEquals(metadata.authors,[["Davies","Louise"],["Kirk","James T."]],"Authors have been set correctly after deleting some");
+        this.test.assertEquals(metadata.referencedBy,["URL1","URL4"],"ReferencedBy had been set correctly after deleting some");
     });
     
 });
