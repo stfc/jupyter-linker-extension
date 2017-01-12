391ff69315bdc3763825a13c490b9c8d26562e3c
Louise Davies <louise.davies@stfc.ac.uk>
2017-01-12 15:58:21 +0000

    Fixed some alignment, fixed an issue with the class of file_tree_button not being correct due to a missing space and moved the menubar modification stuff from notebook to common to fix the css for the edit page

--- linker_extension/static/common/modify_common_html.js
+++ linker_extension/static/common/modify_common_html.js
@@ -3,19 +3,20 @@ define(["base/js/namespace","base/js/utils"],function(Jupyter,utils){
     var load = function() {
         var header_containter = $("#header-container").detach();
         var header_containter_container = $("<div/>")
-                                          .attr("id","header-container-container")
-                                          .addClass("container");
+            .attr("id","header-container-container")
+            .addClass("container");
 
         header_containter_container.append(header_containter);
         $("#header").prepend(header_containter_container);
 
         var file_tree_button = $("<a/>")
-                               .addClass("btn btn-default btn-sm navbar-btn" 
-                                         + "pull-right file-tree-button")
-                               .css("margin-right","2px")
-                               .css("margin-left","2px")
-                               .attr("href",utils.get_body_data("baseUrl"))
-                               .text("File Tree");
+           .attr("id","file-tree-button")
+           .addClass("btn btn-default btn-sm navbar-btn " + 
+                     "pull-right")
+           .css("margin-right","2px")
+           .css("margin-left","2px")
+           .attr("href",utils.get_body_data("baseUrl"))
+           .text("File Tree");
 
         $("#login_widget").after(file_tree_button);
         
@@ -29,6 +30,18 @@ define(["base/js/namespace","base/js/utils"],function(Jupyter,utils){
             .attr("href", utils.get_body_data("baseUrl")
                           + "nbextensions/linker_extension/common/common_style.css")
         );
+
+        //applies to both notebook and edit - put it here so we don't have
+        //to add additional code just for the edit page
+        if($("#menubar-container")) {
+            var menubar_container = $("#menubar-container").detach();
+            var menubar_container_container = $("<div/>")
+                .attr("id","menubar-container-container")
+                .addClass("container");
+            menubar_container_container.append(menubar_container);
+            $(".header-bar").after(menubar_container_container);
+        }
+        
     };
 
     module.exports = {load: load};
