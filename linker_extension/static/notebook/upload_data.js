7fc9765546bfa10c7f812ee16f9e964e34eb3fac
Louise Davies <louise.davies@stfc.ac.uk>
2017-01-12 09:37:43 +0000

    Added bundle url to success notification

--- linker_extension/static/notebook/upload_data.js
+++ linker_extension/static/notebook/upload_data.js
@@ -132,7 +132,8 @@ define([
                     md.databundle_url = id;
                     Jupyter.notebook.save_notebook();
                     custom_utils.create_alert("alert-success",
-                                              "Success! Item created in DSpace via SWORD!")
+                                              "Success! Item created in eData!" +
+                                              "It is located here: " + id)
                                 .attr("item-id",id);
                 },
                 function(reason) {
