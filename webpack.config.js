49c08528265e2627f3dffb006c61d718e94da77b
Louise Davies <louise.davies@stfc.ac.uk>
2017-01-16 11:45:51 +0000

    Added es6-promise to the common entry point, so that Promises are available on all pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-module.exports = {
-	entry: {
-		'linker_extension/static/tree/linker_extension_tree': "./linker_extension/static/tree/tree_index.js",
-		'linker_extension/static/notebook/linker_extension_notebook': "./linker_extension/static/notebook/notebook_index.js",
-		'linker_extension/static/common/linker_extension_common': "./linker_extension/static/common/common_index.js",
-	},
-	output: {
-		path: "./",
-		filename: "[name].js",
-		libraryTarget: "amd",
-	},
 
-	externals: {
-		"base/js/namespace": "base/js/namespace",
-		"base/js/utils": "base/js/utils",
-		"base/js/dialog": "base/js/dialog",
-		"base/js/events": "base/js/events",
-		"notebook/js/celltoolbar": "notebook/js/celltoolbar",
-	}
+module.exports = {
+    entry: {
+        "linker_extension/static/tree/linker_extension_tree": "./linker_extension/static/tree/tree_index.js",
+        "linker_extension/static/notebook/linker_extension_notebook": "./linker_extension/static/notebook/notebook_index.js",
+        "linker_extension/static/common/linker_extension_common": ["es6-promise/auto","./linker_extension/static/common/common_index.js"],
+
+    },
+    output: {
+        path: "./",
+        filename: "[name].js",
+        libraryTarget: "amd",
+    },
+
+    externals: {
+        "base/js/namespace": "base/js/namespace",
+        "base/js/utils": "base/js/utils",
+        "base/js/dialog": "base/js/dialog",
+        "base/js/events": "base/js/events",
+        "notebook/js/celltoolbar": "notebook/js/celltoolbar",
+    }
 };
