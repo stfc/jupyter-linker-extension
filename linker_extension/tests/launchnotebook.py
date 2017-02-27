7d3ecb996e12f28c240acc7512aa3fb609878a65
Louise Davies <louise.davies@stfc.ac.uk>
2017-02-27 15:42:04 +0000

    Use tempfile because genutils is legacy. Updated from notebook master

--- linker_extension/tests/launchnotebook.py
+++ linker_extension/tests/launchnotebook.py
@@ -2,12 +2,13 @@
 
 from __future__ import print_function
 
+from binascii import hexlify
+from contextlib import contextmanager
+import errno
 import os
 import sys
-import time
-import requests
-from contextlib import contextmanager
 from threading import Thread, Event
+import time
 from unittest import TestCase
 
 pjoin = os.path.join
@@ -17,13 +18,15 @@ try:
 except ImportError:
     from mock import patch #py2
 
+import requests
 from tornado.ioloop import IOLoop
 import zmq
 
 import jupyter_core.paths
 from traitlets.config import Config
 from ..notebookapp import NotebookApp
-from ipython_genutils.tempdir import TemporaryDirectory
+from ..utils import url_path_join
+from tempfile import TemporaryDirectory
 
 MAX_WAITTIME = 30   # seconds to wait for notebook server to start
 POLL_INTERVAL = 0.1 # time between attempts
@@ -35,7 +38,6 @@ class TimeoutError(Exception):
 
 class NotebookTestBase(TestCase):
     """A base class for tests that need a running notebook.
-
     This create some empty config and runtime directories
     and then starts the notebook server with them.
     """
@@ -68,26 +70,67 @@ class NotebookTestBase(TestCase):
         cls.notebook_thread.join(timeout=MAX_WAITTIME)
         if cls.notebook_thread.is_alive():
             raise TimeoutError("Undead notebook server")
+    
+    @classmethod
+    def auth_headers(cls):
+        headers = {}
+        if cls.token:
+            headers['Authorization'] = 'token %s' % cls.token
+        return headers
 
     @classmethod
+    def request(cls, verb, path, **kwargs):
+        """Send a request to my server
+        
+        with authentication and everything.
+        """
+        headers = kwargs.setdefault('headers', {})
+        headers.update(cls.auth_headers())
+        response = requests.request(verb,
+            url_path_join(cls.base_url(), path),
+            **kwargs)
+        return response
+    
+    @classmethod
     def setup_class(cls):
-        cls.home_dir = TemporaryDirectory()
-        data_dir = TemporaryDirectory()
+        cls.tmp_dir = TemporaryDirectory()
+        def tmp(*parts):
+            path = os.path.join(cls.tmp_dir.name, *parts)
+            try:
+                os.makedirs(path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+            return path
+        
+        cls.home_dir = tmp('home')
+        data_dir = cls.data_dir = tmp('data')
+        config_dir = cls.config_dir = tmp('config')
+        runtime_dir = cls.runtime_dir = tmp('runtime')
+        cls.notebook_dir = tmp('notebooks')
         cls.env_patch = patch.dict('os.environ', {
-            'HOME': cls.home_dir.name,
+            'HOME': cls.home_dir,
             'PYTHONPATH': os.pathsep.join(sys.path),
-            'IPYTHONDIR': pjoin(cls.home_dir.name, '.ipython'),
-            'JUPYTER_DATA_DIR' : data_dir.name
+            'IPYTHONDIR': pjoin(cls.home_dir, '.ipython'),
+            'JUPYTER_NO_CONFIG': '1', # needed in the future
+            'JUPYTER_CONFIG_DIR' : config_dir,
+            'JUPYTER_DATA_DIR' : data_dir,
+            'JUPYTER_RUNTIME_DIR': runtime_dir,
         })
         cls.env_patch.start()
-        cls.path_patch = patch.object(jupyter_core.paths, 'SYSTEM_JUPYTER_PATH', [])
+        cls.path_patch = patch.multiple(
+            jupyter_core.paths,
+            SYSTEM_JUPYTER_PATH=[tmp('share', 'jupyter')],
+            ENV_JUPYTER_PATH=[tmp('env', 'share', 'jupyter')],
+            SYSTEM_CONFIG_PATH=[tmp('etc', 'jupyter')],
+            ENV_CONFIG_PATH=[tmp('env', 'etc', 'jupyter')],
+        )
         cls.path_patch.start()
-        cls.config_dir = TemporaryDirectory()
-        cls.data_dir = data_dir
-        cls.runtime_dir = TemporaryDirectory()
-        cls.notebook_dir = TemporaryDirectory()
+
         config = cls.config or Config()
         config.NotebookNotary.db_file = ':memory:'
+
+        cls.token = hexlify(os.urandom(4)).decode('ascii')
 
         started = Event()
         def start_thread():
@@ -95,13 +138,14 @@ class NotebookTestBase(TestCase):
                 port=cls.port,
                 port_retries=0,
                 open_browser=False,
-                config_dir=cls.config_dir.name,
-                data_dir=cls.data_dir.name,
-                runtime_dir=cls.runtime_dir.name,
-                notebook_dir=cls.notebook_dir.name,
+                config_dir=cls.config_dir,
+                data_dir=cls.data_dir,
+                runtime_dir=cls.runtime_dir,
+                notebook_dir=cls.notebook_dir,
                 base_url=cls.url_prefix,
                 config=config,
                 allow_root=True,
+                token=cls.token,
             )
             # don't register signal handler during tests
             app.init_signal = lambda : None
@@ -130,11 +174,7 @@ class NotebookTestBase(TestCase):
     def teardown_class(cls):
         cls.notebook.stop()
         cls.wait_until_dead()
-        cls.home_dir.cleanup()
-        cls.config_dir.cleanup()
-        cls.data_dir.cleanup()
-        cls.runtime_dir.cleanup()
-        cls.notebook_dir.cleanup()
+        cls.tmp_dir.cleanup()
         cls.env_patch.stop()
         cls.path_patch.stop()
         # cleanup global zmq Context, to ensure we aren't leaving dangling sockets
