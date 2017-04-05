81a84ea488e8bc9c6f76d2a4a120dcb7131fa2ab
Louise Davies <louise.davies@stfc.ac.uk>
2017-04-05 10:10:26 +0100

    Remove requirements on deleted files

--- linker_extension/nbextensions/notebook/notebook_index.js
+++ linker_extension/nbextensions/notebook/notebook_index.js
@@ -3,10 +3,8 @@ require("../custom_utils.js");
 var add_metadata = require("./add_metadata.js");
 var custom_cell_toolbar = require("./custom_cell_toolbar.js");
 var modify_notebook_html = require("./modify_notebook_html.js");
-var select_data_notebook = require("./select_data_notebook.js");
 require("./upload_data.js");
 require("./upload_notebook.js");
-var view_data_dialog = require("./view_data_dialog.js");
 var publish_notebook = require("./publish_notebook.js");
 var generate_references = require("./generate_references.js");
 var download_data = require("./download_data.js");
@@ -35,8 +33,6 @@ function load_ipython_extension(){
 
     modify_notebook_html.load();
     add_metadata.load();
-    select_data_notebook.load();
-    view_data_dialog.load();
     custom_cell_toolbar.load();
     publish_notebook.load();
     generate_references.load();
